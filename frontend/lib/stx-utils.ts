0101ed028eb791167f63287e6dbc764ff1279c1e
Edwin Liava'a <etuini.liavaa@gmail.com>
2025-08-12 13:02:39 +1300

    first commit

--- frontend/lib/stx-utils.ts
+++
@@ -1,7 +0,0 @@
-export function abbreviateAddress(address: string) {
-  return `${address.substring(0, 5)}...${address.substring(36)}`;
-}
-
-export function abbreviateTxnId(txnId: string) {
-  return `${txnId.substring(0, 5)}...${txnId.substring(62)}`;
-}
